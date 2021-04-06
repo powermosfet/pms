9e714030b19e834f96bd7b1b17be23b5fc43abfe
Asmund Berge <herrasbe@gmail.com>
2021-04-06 21:40:12 +0200

    Set line buffering

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,14 +6,16 @@ import Protolude
 import qualified Protolude.Base
 
 import Api (api, server)
-import qualified Config
 import Network.Wai.Handler.Warp (run)
 import Servant (serve)
 import System.Environment (getEnvironment)
+import System.IO (hSetBuffering, stdout, BufferMode(LineBuffering))
+import qualified Config
 
 pms :: IO ()
 pms = do
     env <- getEnvironment
+    hSetBuffering stdout LineBuffering
     case Config.fromEnvironment env of
         Right config -> do
             putStrLn (Protolude.Base.show config)
