f4fb6b0c102b745873ad6ada6b7101f9719611dc
Asmund Berge <herrasbe@gmail.com>
2021-04-07 19:30:34 +0200

    Set content type

--- src/Signal.hs
+++ src/Signal.hs
@@ -9,7 +9,7 @@ import Config (Config(..))
 import Data.Aeson (ToJSON, encode)
 import GHC.Generics (Generic)
 import Memo (Memo(..))
-import Network.HTTP (Response, Request(..), postRequest, simpleHTTP)
+import Network.HTTP (Response, Request(..), postRequest, simpleHTTP, Header(..), HeaderName(HdrContentType))
 import Network.Stream
 import qualified Data.Text as T
 import qualified Data.ByteString.Lazy as BS
@@ -35,7 +35,8 @@ signalMessage (Config {..}) (Memo {..}) =
 sendSignalMsg :: Config -> Memo -> IO (Either ConnError (Response BS.ByteString))
 sendSignalMsg config memo = do
     let r = (postRequest (T.unpack (signalUrl config) ++ "/v2/send")) { rqBody = encode (signalMessage config memo) }
+    let newHeaders = (Header HdrContentType "application/json") : rqHeaders r
     BS.putStrLn (encode (signalMessage config memo))
     print r
-    simpleHTTP r
+    simpleHTTP (r { rqHeaders = newHeaders })
 
